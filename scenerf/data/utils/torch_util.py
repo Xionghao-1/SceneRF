5dc25dbb87669237d39052ca2ad0056660219948
Hao Xiong <145336666+Xionghao-1@users.noreply.github.com>
2024-01-24 23:19:51 +0800

    Update torch_util.py

--- scenerf/data/utils/torch_util.py
+++ scenerf/data/utils/torch_util.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-
+# 初始化每个数据加载器工作进程的随机数生成器种子，以确保每个工作进程具有不同的种子，以避免随机性相关的问题
 def worker_init_fn(worker_id):
     """The function is designed for pytorch multi-process dataloader.
     Note that we use the pytorch random generator to generate a base_seed.
